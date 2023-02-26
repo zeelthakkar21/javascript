0812c7064c8aacfa7befdd17b36e512c1458d03d
zeelthakkar21 <zeelthakkar2111@gmail.com>
2023-02-26 11:07:33 +0530

    js2

---
+++ javascriptfunction/sandbox.js
@@ -0,0 +1,17 @@
+//function expression
+const speak = function(){
+    console.log('good day');
+ };
+ speak();
+
+
+//function declaration
+function greet (){
+    console.log('hello there');
+}
+
+
+
+//greet();  //function call karyu
+//greet();
+//greet();
