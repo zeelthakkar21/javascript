5ad73ccff304f0f115f2090155947bf95250d46c
zeelthakkar21 <zeelthakkar2111@gmail.com>
2023-02-27 17:12:29 +0530

    js4

---
+++ javascriptDOM/sandbox.js
@@ -0,0 +1,15 @@
+//const para=document.querySelector('body > h1'); //copy selector
+//const para=document.querySelector('div.error');
+//console.log(para);
+
+const paras=document.querySelectorAll('p');
+//console.log(paras);
+
+/*paras.forEach(para=>{
+    console.log(para);
+});*/
+
+const errors=document.querySelectorAll('.error');
+console.log(errors);
+
+console.log(paras[0]);
